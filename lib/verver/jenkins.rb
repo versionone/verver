bb925134fae759eed3021fdade72590bf7a198bf
Steven Harman <steveharman@gmail.com>
2012-01-18 17:15:10 -0500

    Jenkins.build_number lives

--- lib/verver/jenkins.rb
+++ lib/verver/jenkins.rb
@@ -3,4 +3,8 @@ class Jenkins
   def self.job_name
     ENV['JOB_NAME'] || 'local-job'
   end
+
+  def self.build_number
+    ENV['BUILD_NUMBER'] || 0
+  end
 end
--- spec/verver/jenkins_spec.rb
+++ spec/verver/jenkins_spec.rb
@@ -8,8 +8,19 @@ describe Jenkins do
     end
 
     it "is overridden by JOB_NAME ENV variable" do
-      ENV.stub(:[]).and_return('bob-job')
+      ENV.stub(:[]).with('JOB_NAME').and_return('bob-job')
       Jenkins.job_name.should == 'bob-job'
+    end
+  end
+
+  describe ".build_number" do
+    it "defaults to 0" do
+      Jenkins.build_number.should == 0
+    end
+
+    it "is overridden by BUILD_NUMBER ENV variable" do
+      ENV.stub(:[]).with('BUILD_NUMBER').and_return(42)
+      Jenkins.build_number.should == 42
     end
   end
 
