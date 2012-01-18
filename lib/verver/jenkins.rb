bd4f4ea82b34e654d2a89b34a0feaa7c0d47294a
Steven Harman <steveharman@gmail.com>
2012-01-18 17:16:17 -0500

    move under Verver namespace

--- lib/verver/jenkins.rb
+++ lib/verver/jenkins.rb
@@ -1,10 +1,12 @@
-class Jenkins
+module Verver
+  class Jenkins
 
-  def self.job_name
-    ENV['JOB_NAME'] || 'local-job'
-  end
+    def self.job_name
+      ENV['JOB_NAME'] || 'local-job'
+    end
 
-  def self.build_number
-    ENV['BUILD_NUMBER'] || 0
+    def self.build_number
+      ENV['BUILD_NUMBER'] || 0
+    end
   end
 end
--- spec/verver/jenkins_spec.rb
+++ spec/verver/jenkins_spec.rb
@@ -1,26 +1,26 @@
 require 'verver/jenkins'
 
-describe Jenkins do
+describe Verver::Jenkins do
 
   describe ".job_name" do
     it "defaults to 'local-job'" do
-      Jenkins.job_name.should == 'local-job'
+      Verver::Jenkins.job_name.should == 'local-job'
     end
 
     it "is overridden by JOB_NAME ENV variable" do
       ENV.stub(:[]).with('JOB_NAME').and_return('bob-job')
-      Jenkins.job_name.should == 'bob-job'
+      Verver::Jenkins.job_name.should == 'bob-job'
     end
   end
 
   describe ".build_number" do
     it "defaults to 0" do
-      Jenkins.build_number.should == 0
+      Verver::Jenkins.build_number.should == 0
     end
 
     it "is overridden by BUILD_NUMBER ENV variable" do
       ENV.stub(:[]).with('BUILD_NUMBER').and_return(42)
-      Jenkins.build_number.should == 42
+      Verver::Jenkins.build_number.should == 42
     end
   end
 
