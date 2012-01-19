77d255ed3539940b33416917e801f3d3f8a61fda
Steven Harman <steveharman@gmail.com>
2012-01-19 14:48:30 -0500

    refactor jenkins specs

--- spec/verver/jenkins_spec.rb
+++ spec/verver/jenkins_spec.rb
@@ -3,25 +3,29 @@ require 'verver/jenkins'
 describe Verver::Jenkins do
 
   describe ".job_name" do
+    let(:stub_job_name) { ENV.stub(:[]).with('JOB_NAME') }
+
     it "defaults to 'local-job'" do
-      ENV.stub(:[]).with('JOB_NAME').and_return(nil)
+      stub_job_name.and_return(nil)
       Verver::Jenkins.job_name.should == 'local-job'
     end
 
     it "is overridden by JOB_NAME ENV variable" do
-      ENV.stub(:[]).with('JOB_NAME').and_return('bob-job')
+      stub_job_name.and_return('bob-job')
       Verver::Jenkins.job_name.should == 'bob-job'
     end
   end
 
   describe ".build_number" do
+    let(:stub_build_number) { ENV.stub(:[]).with('BUILD_NUMBER') }
+
     it "defaults to 0" do
-      ENV.stub(:[]).with('BUILD_NUMBER').and_return(nil)
+      stub_build_number.and_return(nil)
       Verver::Jenkins.build_number.should == 0
     end
 
     it "is overridden by BUILD_NUMBER ENV variable" do
-      ENV.stub(:[]).with('BUILD_NUMBER').and_return(42)
+      stub_build_number.and_return(42)
       Verver::Jenkins.build_number.should == 42
     end
   end
