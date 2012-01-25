817b185e3589d5b08d6987f9ba5a9e6aa503bb1f
Steven Harman <steveharman@gmail.com>
2012-01-25 15:12:49 -0500

    Verver::IIS::Automation delegates to WIN32OLE obj

--- lib/verver/iis/automation.rb
+++ lib/verver/iis/automation.rb
@@ -8,6 +8,11 @@ end
 module Verver
   module IIS
 
+    # A thin wrapper around a WIN32OLE object that will automate
+    # management of IIS.
+    #
+    # iis_automation = Automation.new
+    # app_pool = iis_automation.get("ApplicationPool.name='DefaultAppPool'")
     class Automation < SimpleDelegator
       ADMIN_MONIKER = 'winmgmts:root\\WebAdministration'.freeze
 
--- spec/verver/iis/automation_spec.rb
+++ spec/verver/iis/automation_spec.rb
@@ -1,15 +1,24 @@
 require 'verver/iis/automation'
 
 describe Verver::IIS::Automation do
-  describe "a new IIS Automation" do
-    let(:win32ole) { stub("WIN32OLE") }
-    let(:iis_automation) { stub("WIN32OLE - WebAdministration") }
+  let(:win32ole_fake) { stub("WIN32OLE") }
+  let(:iis_automation) { stub("WIN32OLE - WebAdministration") }
 
+  describe "a new IIS Automation" do
     it "attaches to the IIS Automation object" do
-      win32ole.should_receive(:connect).
+      win32ole_fake.should_receive(:connect).
         with(Verver::IIS::Automation::ADMIN_MONIKER) { iis_automation }
 
-      Verver::IIS::Automation.new(win32ole)
+      Verver::IIS::Automation.new(win32ole_fake)
+    end
+  end
+
+  describe "using an IIS Automation" do
+    it "passes messages onto the Win32ole-based IIS Administration object" do
+      win32ole_fake.stub(:connect) { iis_automation }
+      iis_automation.should_receive(:get).with("SomeKey='MyValue'")
+
+      Verver::IIS::Automation.new(win32ole_fake).get("SomeKey='MyValue'")
     end
 
   end
