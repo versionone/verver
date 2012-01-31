f855b91f4d67edbd54c4038c32e0140b9e6df8af
Steven Harman <steveharman@gmail.com>
2012-01-31 13:58:59 -0500

    delegate AppPools to the WIN32OLE object

--- lib/verver/iis/app_pool.rb
+++ lib/verver/iis/app_pool.rb
@@ -1,3 +1,5 @@
+require 'delegate'
+
 module Verver
   module IIS
 
@@ -7,10 +9,23 @@ module Verver
     # app_pool.stop
     # app_pool.start
     # app_pool.recycle
-    class AppPool
+    class AppPool < SimpleDelegator
 
       def initialize(name, iis_automation=Automation.new)
-        @app_pool = iis_automation.get(app_pool_key(name))
+        app_pool = iis_automation.get(app_pool_key(name))
+        super(app_pool)
+      end
+
+      def method_missing(name, *args)
+        if __getobj__.ole_respond_to?(name)
+          __getobj__.send(name, *args)
+        else
+          super(name, *args)
+        end
+      end
+
+      def respond_to_missing?(name, include_private)
+        ole_respond_to?(name) || super(name, include_private)
       end
 
       private
--- spec/verver/iis/app_pool_spec.rb
+++ spec/verver/iis/app_pool_spec.rb
@@ -1,13 +1,23 @@
 require 'verver/iis/app_pool'
 
 describe Verver::IIS::AppPool do
-  describe "A new AppPool" do
-    let(:iis_automation) { stub("WIN32OLE - WebAdministration") }
+  let(:iis_automation) { stub("WIN32OLE - WebAdministration") }
 
+  describe "A new AppPool" do
     it "is tied to the named pool" do
       iis_automation.should_receive(:get).with("ApplicationPool.Name='Bob\'s Pool'")
-      app_pool = Verver::IIS::AppPool.new("Bob's Pool", iis_automation)
-      #app_pool.name.should == "Bob's Pool"
+      Verver::IIS::AppPool.new("Bob's Pool", iis_automation)
+    end
+  end
+
+  describe "using an AppPool" do
+    let(:app_pool) { stub("WIN32OLE - AppPool") }
+    it "passes messages onto the Win32ole-based IIS Administration object" do
+      iis_automation.stub(:get) { app_pool }
+      app_pool.stub(:ole_respond_to?) { true }
+      app_pool.should_receive(:start)
+
+      Verver::IIS::AppPool.new("Bob's Pool", iis_automation).start
     end
   end
 end
