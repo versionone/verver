7c6e2f29c970ae587efab5453b2968dccc167bab
David Laribee <laribee@gmail.com>
2012-02-15 13:51:02 -0500

    remove old api class

--- lib/verver/loader.rb
+++ lib/verver/loader.rb
@@ -5,7 +5,6 @@ require 'verver/loader/config'
 require 'verver/loader/utility'
 require 'verver/loader/path_builder'
 require 'verver/loader/asset'
-require 'verver/loader/api'
 require 'verver/loader/api2'
 require 'verver/loader/dsl'
 
